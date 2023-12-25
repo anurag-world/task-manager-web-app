c951f32ba2352a518b0b345f2f2835905be47c9b
Anurag V <47092363+anurag-world@users.noreply.github.com>
2023-12-25 15:31:11 +0530

    Add MongoDb

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.json());
 
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/taskManager")
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
 let tasks = [];
 
 // Get all tasks
-app.get('/tasks', (req, res) => {
+app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
 // Add a new task
-app.post('/tasks', (req, res) => {
+app.post("/tasks", (req, res) => {
   const { task } = req.body;
   tasks.push(task);
-  res.status(201).json({ message: 'Task added successfully' });
+  res.status(201).json({ message: "Task added successfully" });
 });
 
 // Delete a task
-app.delete('/tasks/:id', (req, res) => {
+app.delete("/tasks/:id", (req, res) => {
   const taskId = parseInt(req.params.id);
   tasks.splice(taskId, 1);
-  res.status(200).json({ message: 'Task deleted successfully' });
+  res.status(200).json({ message: "Task deleted successfully" });
 });
 
 const PORT = 3000;
